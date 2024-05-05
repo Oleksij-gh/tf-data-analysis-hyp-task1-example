52e3af29e00b874b5e20e2305a4aa1e60fa53875
Aleksei <72971918+Oleksij-gh@users.noreply.github.com>
2024-05-05 22:25:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,22 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind_from_stats
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 581150379 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+
+    alpha=0.05
+    
+    x_mean = x_success / x_cnt
+    y_mean = y_success / y_cnt
+    # Стандартные ошибки для обеих групп
+    x_se = math.sqrt(x_mean * (1 - x_mean) / x_cnt)
+    y_se = math.sqrt(y_mean * (1 - y_mean) / y_cnt)
+    # t-тест
+    stat, p_value = ttest_ind_from_stats(x_mean, x_se, x_cnt, y_mean, y_se, y_cnt, equal_var=False)
+               
+    return p_value < alpha
